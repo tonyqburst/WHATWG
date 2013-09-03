796bdf5fcec4e21d1411737b4dca332b720aa4e9
Tony Jose <tony@qburst.com>
2013-09-03 12:24:25 +0530

    Added project files

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('WHATWGApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
