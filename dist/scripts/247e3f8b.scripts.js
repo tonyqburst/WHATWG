a3660efc15e8efc05b19630dfa6026bd1fea9cb3
Tony Jose <tony@qburst.com>
2013-09-03 13:05:39 +0530

    Added fist and all files

---
+++ dist/scripts/247e3f8b.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("WHATWGApp",[]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("WHATWGApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
